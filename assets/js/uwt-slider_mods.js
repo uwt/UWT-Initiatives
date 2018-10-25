eeca33718730ebd5f5da98f618020c89e4b4f05b
jamesini <gurufoo@gmail.com>
2018-10-25 16:20:49 -0700

    Fixed links from initiatives home -> initiative slider for the equity impact goal.

---
+++ assets/js/uwt-slider_mods.js
@@ -0,0 +1,14 @@
+/*
+* Manipulate the links to the initiative-[impact-goal].html pages
+* on the initiatives.html page.
+* */
+(function () {
+    'use strict';
+
+    // Count the slides, less 1 for the overview slide
+    let bobo = $("#rev_slider > ul > li").length - 1;
+
+
+
+
+}());
