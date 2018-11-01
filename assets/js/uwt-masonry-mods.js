c4e8fe6c0d7ec61b35c41d87ca9a9f5dc7dd1f70
jamesini <gurufoo@gmail.com>
2018-11-01 09:23:42 -0700

    Removed log messages from uwt-masonry-mods.js file.

--- assets/js/uwt-masonry-mods.js
+++ assets/js/uwt-masonry-mods.js
@@ -7,10 +7,10 @@
     // Clicking on an item filter also displays a button link which corresponds
     // to the item filter clicked.
     $('.items-filter a').click(function () {
-        console.log('I have been clicked!!');
+       /* console.log('I have been clicked!!');*/
         $('.init-links div').fadeOut(300);
         let selector = '.init-links div' + $(this).attr('data-filter');
-        console.log(selector);
+       /* console.log(selector);*/
 
         setTimeout(function () {
             $(selector).fadeIn(200);
